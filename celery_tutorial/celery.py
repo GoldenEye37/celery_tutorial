b1fcca3c48a1190eb4c0f5dc7724a856d7208d39
LDhimba <Ldhimba@bancabc.co.zw>
2022-12-21 11:58:11 +0200

    create task

--- celery_tutorial/celery.py
+++ celery_tutorial/celery.py
@@ -16,3 +16,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registred Django app configs.
 app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
